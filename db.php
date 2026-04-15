c47a132177f96fbac2d33927fadfdc041bb19b84
trojetislem <trojetislem8@gmail.com>
2026-04-15 23:55:33 +0100

    modification du nom de la base dans db.php

--- db.php
+++ db.php
@@ -2,9 +2,8 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db   = "hotel_mahari";
+$db   = "hotel_el_mahari";
 $conn = mysqli_connect($host, $user, $pass, $db);
-// Vérification de la connexion
 if (!$conn) {
     die("Connexion impossible : " . mysqli_connect_error());
 }
